aaa5ed63aa477b1e74d71f3707b47140477e10a1
Shanksdb <akashmadness109@gmail.com>
2025-02-12 00:44:53 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -34,24 +34,25 @@ from os import path, getenv
 import os, time 
 
 class Config:
-    API_ID = int(getenv("API_ID", "0112234"))
-    API_HASH = getenv("API_HASH", "abcdefg")
-    BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:rkndeveloperDEhdhyjjvjjftSEW")
+    API_ID = int(getenv("API_ID", "28713982"))
+    API_HASH = getenv("API_HASH", "237e15f7c006b10b4fa7c46fee7a5377")
+    BOT_TOKEN = getenv("BOT_TOKEN", "")
  
-    FORCE_SUB = int(getenv("FORCE_SUB", "-1001819787652"))
-    ADMIN = list(map(int, getenv("ADMIN", "6151758586").split()))
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002120760645"))
+    FORCE_SUB = int(getenv("FORCE_SUB", "-1002100963256"))
+    ADMIN = list(map(int, getenv("ADMIN", "7195990500").split()))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002100963256"))
     
     # database configs
     DB_URL = os.environ.get("DB_URL", "")
-    DB_NAME = os.environ.get("DB_NAME", "")
+    DB_NAME = os.environ.get("DB_NAME", "Inflixacceptbot")
     
     #web response 
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
     BOT_UPTIME  = time.time()
     PORT = os.environ.get("PORT", "8080")
     
-    RKN_PIC = os.environ.get("RKN_PIC", "https://graph.org/file/e846f9375e9d4f4975ce4.jpg")
+    RKN_PIC = os.environ.get("RKN_PIC", "https://envs.sh/4f8.jpg
+")
     
  # Subsprice Gif & Auto Request Accept
     SURPRICE = os.environ.get("SURPRICE", "https://telegra.ph/file/a5a2bb456bf3eecdbbb99.mp4 https://telegra.ph/file/03c6e49bea9ce6c908b87.mp4 https://telegra.ph/file/9ebf412f09cd7d2ceaaef.mp4 https://telegra.ph/file/293cc10710e57530404f8.mp4 https://telegra.ph/file/506898de518534ff68ba0.mp4 https://telegra.ph/file/dae0156e5f48573f016da.mp4 https://telegra.ph/file/3e2871e714f435d173b9e.mp4 https://telegra.ph/file/714982b9fedfa3b4d8d2b.mp4 https://telegra.ph/file/876edfcec678b64eac480.mp4 https://telegra.ph/file/6b1ab5aec5fa81cf40005.mp4 https://telegra.ph/file/b4834b434888de522fa49.mp4").split()
